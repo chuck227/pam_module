d336352457b27ad83b96fb97a0ed6a882a334756
Charles Tucker <charlie@DESKTOP-AMT0VJB.localdomain>
2023-07-17 16:28:08 -0400

    Begun keyboard streak changes

--- Makefile
+++ Makefile
@@ -1,4 +1,10 @@
-all: library 
+all: library test
 
 library:
 	gcc -o pam_password_verifier.so -shared -fPIC pam_password_verifier.c -lpam
+
+test: 
+	gcc -o test test.c 
+
+clean:
+	rm test pam_password_verifier.so
--- pam_password_verifier.c
+++ pam_password_verifier.c
@@ -1,2 +1,65 @@
 #include <security/pam_modules.h>
+#include <ctype.h>
 
+#include "pam_password_verifier.h"
+
+int char_to_keyboard_value(char var){
+	switch(var){
+		case '!': 
+			var='1';
+			break;
+		case '@':
+			var='2';
+			break;
+		case '#':
+			var='3';
+			break;
+		case '$':
+			var='4';
+			break;
+		case '%':
+			var='5';
+			break;
+		case '^':
+			var='6';
+			break;
+	}
+
+
+	/*switch(var){
+		case '=' || '+':
+			char_value+=1;
+		case '-' || '_':
+			char_value+=1;
+		case '0' || ')':
+			char_value+=1;
+		case '9' || '(':
+			char_value+=1;
+
+		case '1' || '!':
+			char_value+=1;
+			break
+
+		case 'p':
+			char_value+=1;
+		case 'o': 
+			char_value+=1;
+		case 'i':
+			char_value+=1;
+		case 'u':
+			char_value+=1;
+		case 'y':
+			char_value+=1;
+		case 't':
+			char_value+=1;
+		case 'r':
+			char_value+=1;
+		case 'e':
+			char_value+=1;
+		case 'w':
+			char_value+=1;
+		case 'q':
+			char_value+=101;
+			break;
+	}*/
+}
